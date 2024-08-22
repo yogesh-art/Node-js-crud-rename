ab1a07bdbe35b1c0de4d769db6d6b97cc70322d0
Priyanka <priyanka.panjabi8@gmail.com>
2024-08-22 15:43:37 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// const fs = require('fs');
+
+// const path = require('path');
+// const dirpath = path.join(__dirname,'crud');
+
+// fs.writeFileSync(`${dirpath}/hello.txt`,'I am simple file');
+
+// fs.readFile(`${dirpath}/hello.txt`,'utf8',(err,item)=>{
+//     console.log(item);
+// });
+
+// fs.appendFile(path.join(__dirname,'crud') + "/" + 'hello.txt',' and my name is Yogesh Panjabi',(err)=>{
+//     if(!err){
+//         console.log("File has been updated");
+//     }
+// });
+
+// fs.rename(path.join(__dirname,'crud') + "/" + "hello.txt",path.join(__dirname,'crud') + "/" + "apple.txt",(err)=>{
+//     if(!err){
+//         console.log("File name has been updated");
+//     }
+// });
+
+// fs.unlinkSync(path.join(__dirname,'crud')+'/'+'apple.txt');
